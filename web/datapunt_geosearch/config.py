20ae9aa4f3ba730388e32a5d8346853346750057
Richard Jansen <richard.jansen@lajos.nl>
2016-04-29 16:15:17 +0200

    TAIGA-203 db settings to reflect openstack env

--- web/datapunt_geosearch/config.py
+++ web/datapunt_geosearch/config.py
@@ -10,15 +10,15 @@ local = {
 DSN_ATLAS = 'postgresql://{}:{}@{}:{}/{}'.format(
     os.getenv('ATLAS_DB_USER', 'postgres'),
     os.getenv('ATLAS_DB_PASSWORD', 'insecure'),
-    os.getenv('ATLAS_DB_HOST', 'localhost'),
-    os.getenv('ATLAS_DB_PORT', 5434),
+    os.getenv('ATLAS_DB_PORT_5432_TCP_ADDR', 'localhost'),
+    os.getenv('ATLAS_DB_PORT_5432_TCP_PORT', 5434),
     os.getenv('ATLAS_DB_NAME', 'atlas'),
 )
 
 DSN_NAP = 'postgresql://{}:{}@{}:{}/{}'.format(
     os.getenv('NAP_DB_USER', 'postgres'),
     os.getenv('NAP_DB_PASSWORD', 'insecure'),
-    os.getenv('NAP_DB_HOST', 'localhost'),
-    os.getenv('NAP_DB_PORT', 5401),
+    os.getenv('NAP_DB_PORT_5432_TCP_ADDR', 'localhost'),
+    os.getenv('NAP_DB_PORT_5432_TCP_PORT', 5401),
     os.getenv('NAP_DB_NAME', 'nap'),
 )
