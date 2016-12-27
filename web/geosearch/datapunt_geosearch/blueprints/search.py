c7f0b2d017fd34881ff4abc1e422a0ac4456538b
Kfir Breger <kfir@cubicalwave.com>
2016-12-27 14:42:39 +0100

    tg-476 adding search api for specific item at given radius

--- web/geosearch/datapunt_geosearch/blueprints/search.py
+++ web/geosearch/datapunt_geosearch/blueprints/search.py
@@ -9,9 +9,78 @@ from datapunt_geosearch.datasource import NapMeetboutenDataSource
 search = Blueprint('search', __name__)
 
 
+def get_coords_and_type(args):
+    """
+    Retrieves the coordinates from the request
+    and identifies if the request is in RD or in
+    wgs84
+    If no coordinates are found an error message is set in a
+    response dict.
+    The RD flag is set to true if the coords
+    given are in rd.
 
-@search.route('/', methods=['GET'])
+    @params
+    args - The request args
+
+    @Returns
+    A tuple with the x, y, rd flag and response dict
+    """
+    resp, rd = None, True
+
+    x = args.get('x')
+    y = args.get('y')
+
+    if not x or not y:
+        x = args.get('lat')
+        y = args.get('lon')
+
+        if x and y:
+            rd = False
+        else:
+            resp = {'error': 'No coordinates found'}
+
+    return (x, y, rd, resp)
+
+
+@search.route('/search/', methods=['GET', 'OPTIONS'])
+def search_in_radius():
+    """
+    General geosearch endpoint.
+    Required arguments:
+    x/y or lat/lon for position
+    radius - search radius in meters
+    item - Search item. adressen, meetbouten, kadastrale_objecten etc
+    """
+    x, y, rd, resp = get_coords_and_type(request.args)
+    # If no coords are given, return
+    if resp:
+        return jsonify(resp)
+
+    # Checking for radius and item type
+    radius = request.args.get('radius')
+    if not radius:
+        return jsonify({'error': 'No radius found'})
+    item = request.args.get('item')
+    if not item:
+        return jsonify({'error': 'No item type found'})
+
+    # Got coords, radius and item. Time to search
+    if item in ['peilmerk', 'meetbout']:
+        ds = NapMeetboutenDataSource(dsn=current_app.config['DSN_NAP'])
+    else:
+        ds = AtlasDataSource(dsn=current_app.config['DSN_ATLAS'])
+    # Filtering to the required dataset
+    known_dataset = ds.filter_dataset(item)
+    if not known_dataset:
+        return jsonify({'error': 'Unknown item type'})
+
+    resp = ds.query(float(x), float(y), rd=rd, radius=radius)
+    return jsonify(resp)
+
+
+@search.route('/help/', methods=['GET', 'OPTIONS'])
 def help():
+    print(current_app.config)
     """Help text en query index"""
     return json.dumps({
         '/nap': 'Search in a radius around a point in nap',
@@ -22,20 +91,8 @@ def help():
 @search.route('/nap/', methods=['GET', 'OPTIONS'])
 def search_geo_nap():
     """Performing a geo search for radius around a point using postgres"""
-    resp, rd = None, True
-
-    x = request.args.get('x')
-    y = request.args.get('y')
-
-    if not x or not y:
-        x = request.args.get('lat')
-        y = request.args.get('lon')
-
-        if x and y:
-            rd = False
-        else:
-            resp = {'error': 'No coordinates found'}
-
+    x, y, rd, resp = get_coords_and_type(request.args)
+    print(resp)
     # If no error is found, query
     if not resp:
         ds = NapMeetboutenDataSource(dsn=current_app.config['DSN_NAP'])
@@ -47,19 +104,7 @@ def search_geo_nap():
 @search.route('/atlas/', methods=['GET', 'OPTIONS'])
 def search_geo_atlas():
     """Performing a geo search for radius around a point using postgres"""
-    resp, rd = None, True
-
-    x = request.args.get('x')
-    y = request.args.get('y')
-
-    if not x or not y:
-        x = request.args.get('lat')
-        y = request.args.get('lon')
-
-        if x and y:
-            rd = False
-        else:
-            resp = {'error': 'No coordinates found'}
+    x, y, rd, resp = get_coords_and_type(request.args)
 
     # If no error is found, query
     if not resp:
--- web/geosearch/datapunt_geosearch/datasource.py
+++ web/geosearch/datapunt_geosearch/datasource.py
@@ -33,6 +33,27 @@ class DataSourceBase(object):
 
         self.dsn = dsn
 
+    def filter_dataset(self, dataset_table):
+        """
+        Filters down the dataset to be just the given dataset
+        Expected parameter is dataset name that correlates to the key
+        in the dataset table mapping
+        """
+        filtered_dataset = None
+        print(self.meta)
+        for dataset_name, datasets in self.meta['datasets'].items():
+            for dataset_ident, table in datasets.items():
+                if dataset_ident == dataset_table:
+                    self.meta['datasets'] = {
+                        dataset_name: {
+                            dataset_ident: table
+                        }
+                    }
+                    return True
+
+        self.meta['datasets'] = None
+        return False
+
     def get_cursor(self, conn):
         return conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
@@ -63,12 +84,14 @@ class DataSourceBase(object):
                                 (prop, row[prop])
                                 for prop in self.default_properties if prop in row])
                         })
+
         # Closing the connection to the db
         conn.close()
 
         return features
 
-    def execute_point_query(self, cur, table):
+    # Point query
+    def execute_point_query(self, cur, table): 
         if not self.use_rd:
             sql = """
 SELECT *
@@ -118,71 +141,37 @@ ST_Contains({}, ST_GeomFromText(\'POINT(%s %s)\', 28992))
 
 
 class AtlasDataSource(DataSourceBase):
-    meta = {
-        'geofield': 'geometrie',
-        'operator': 'contains',
-        'datasets': {
-            'bag': {
-                'openbareruimte': 'public.geo_bag_openbareruimte_mat',
-                'pand': 'public.geo_bag_pand_mat',
-                'ligplaats': 'public.geo_bag_ligplaats_mat',
-                'standplaats': 'public.geo_bag_standplaats_mat',
+    def __init__(self, *args, **kwargs):
+        super(AtlasDataSource, self).__init__(*args, **kwargs)
+        self.meta = {
+            'geofield': 'geometrie',
+            'operator': 'contains',
+            'datasets': {
+                'bag': {
+                    'openbareruimte': 'public.geo_bag_openbareruimte_mat',
+                    'pand': 'public.geo_bag_pand_mat',
+                    'ligplaats': 'public.geo_bag_ligplaats_mat',
+                    'standplaats': 'public.geo_bag_standplaats_mat',
+                },
+                'gebieden': {
+                    'stadsdeel': 'public.geo_bag_stadsdeel_mat',
+                    'buurt': 'public.geo_bag_buurt_mat',
+                    'buurtcombinatie': 'public.geo_bag_buurtcombinatie_mat',
+                    'bouwblok': 'public.geo_bag_bouwblok_mat',
+                    'grootstedelijkgebied': 'public.geo_bag_grootstedelijkgebied_mat',
+                    'gebiedsgerichtwerken': 'public.geo_bag_gebiedsgerichtwerken_mat',
+                    'unesco': 'public.geo_bag_unesco_mat',
+                },
+                'lki': {
+                    'kadastraal_object': 'public.geo_lki_kadastraalobject_mat',
+                },
+                'wkpb': {
+                    'beperking': 'public.geo_wkpb_mat',
+                },
             },
-            'gebieden': {
-                'stadsdeel': 'public.geo_bag_stadsdeel_mat',
-                'buurt': 'public.geo_bag_buurt_mat',
-                'buurtcombinatie': 'public.geo_bag_buurtcombinatie_mat',
-                'bouwblok': 'public.geo_bag_bouwblok_mat',
-                'grootstedelijkgebied': 'public.geo_bag_grootstedelijkgebied_mat',
-                'gebiedsgerichtwerken': 'public.geo_bag_gebiedsgerichtwerken_mat',
-                'unesco': 'public.geo_bag_unesco_mat',
-            },
-            'lki': {
-                'kadastraal_object': 'public.geo_lki_kadastraalobject_mat',
-            },
-            'wkpb': {
-                'beperking': 'public.geo_wkpb_mat',
-            },
-        },
-    }
+        }
 
-    def query(self, x, y, rd=True):
-        self.use_rd = rd
-        self.x = x
-        self.y = y
-
-        try:
-            return {
-                'type': 'FeatureCollection',
-                'features': self.execute_queries()
-            }
-        except DataSourceException as err:
-            return {
-                'type': 'Error',
-                'message': 'Error executing query: %s' % err.message
-            }
-        except ProgrammingError as err:
-            return {
-                'type': 'Error',
-                'message': 'Error in database integrity: %s' % repr(err)
-            }
-
-
-class NapMeetboutenDataSource(DataSourceBase):
-    meta = {
-        'geofield': 'geometrie',
-        'operator': 'within',
-        'datasets': {
-            'nap': {
-                'peilmerk': 'public.geo_nap_peilmerk_mat',
-            },
-            'meetbouten': {
-                'meetbout': 'public.geo_meetbouten_meetbout_mat',
-            },
-        },
-    }
-
-    def query(self, x, y, radius=None, rd=True):
+    def query(self, x, y, rd=True, radius=None):
         self.use_rd = rd
         self.x = x
         self.y = y
@@ -205,3 +194,56 @@ class NapMeetboutenDataSource(DataSourceBase):
                 'type': 'Error',
                 'message': 'Error in database integrity: %s' % repr(err)
             }
+        except TypeError as err:
+            return {
+                'type': 'Error',
+                'message': 'Error in handling, {}'.format(repr(err))
+            }
+
+
+class NapMeetboutenDataSource(DataSourceBase):
+
+    def __init__(self, *args, **kwargs):
+        super(NapMeetboutenDataSource, self).__init__(*args, **kwargs)
+        self.meta = {
+            'geofield': 'geometrie',
+            'operator': 'within',
+            'datasets': {
+                'nap': {
+                    'peilmerk': 'public.geo_nap_peilmerk_mat',
+                },
+                'meetbouten': {
+                    'meetbout': 'public.geo_meetbouten_meetbout_mat',
+                },
+            },
+        }
+
+    def query(self, x, y, rd=True, radius=None):
+        self.use_rd = rd
+        self.x = x
+        self.y = y
+
+        if radius:
+            self.radius = radius
+
+        try:
+            return {
+                'type': 'FeatureCollection',
+                'features': self.execute_queries()
+            }
+        except DataSourceException as err:
+            return {
+                'type': 'Error',
+                'message': 'Error executing query: %s' % err.message
+            }
+        except ProgrammingError as err:
+            return {
+                'type': 'Error',
+                'message': 'Error in database integrity: %s' % repr(err)
+            }
+        except TypeError as err:
+            return {
+                'type': 'Error',
+                'message': 'Error in handling, {}'.format(repr(err))
+            }
+
