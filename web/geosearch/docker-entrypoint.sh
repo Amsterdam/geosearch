234c49bfc1c49e129a8423c376bf64fb83b2c544
Jeroen van 't Hert <jeroen@yottabyte.io>
2017-01-06 15:52:19 +0100

    TG-505 Fix ttl issues by registering locally

--- web/geosearch/docker-entrypoint.sh
+++ web/geosearch/docker-entrypoint.sh
@@ -19,5 +19,9 @@ else
   export DOCKER_EXTERNAL_IP=$(curl ${METADATA_URL})
 fi
 
+if [ "${CONSUL_HOST:-notset}" == 'notset' ]; then
+    export CONSUL_HOST=${DOCKER_EXTERNAL_IP}
+fi
+
 echo Starting server
 uwsgi --ini uwsgi.ini
