baf802a245c9a47c6b2fefc46b9bcf943e08a323
Jeroen van 't Hert <jeroen@yottabyte.io>
2017-03-01 14:38:36 +0100

    Docker-Compose 2.x Compatability and possibility for overrides.

--- web/geosearch/datapunt_geosearch/config.py
+++ web/geosearch/datapunt_geosearch/config.py
@@ -2,54 +2,114 @@
 Contains the different configs for the datapunt geosearch application
 """
 import os
-import re
 
 
-def get_docker_host():
+def in_docker():
     """
-    Looks for the DOCKER_HOST environment variable to find the VM
-    running docker-machine.
-
-    If the environment variable is not found, it is assumed that
-    you're running docker on localhost.
+    Checks pid 1 cgroup settings to check with reasonable certainty we're in a
+    docker env.
+    :return: true when running in a docker container, false otherwise
     """
-    d_host = os.getenv('DOCKER_HOST', None)
-    if d_host:
-        if re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', d_host):
-            return d_host
+    # noinspection PyBroadException
+    try:
+        return ':/docker/' in open('/proc/1/cgroup', 'r').read()
+    except:
+        return False
 
-        return re.match(r'tcp://(.*?):\d+', d_host).group(1)
-    return os.getenv('NAP_DB_PORT_5432_TCP_ADDR', 'localhost')
+
+def get_var(db, varname, default):
+    return os.getenv(f"{db}_DB_{varname}_OVERRIDE".upper(), default)
+
+
+DB_SETTINGS = {
+    'ATLAS': {
+        'host': get_var('atlas', 'host', 'localhost'),
+        'port': get_var('atlas', 'port', 5405),
+        'database': get_var('atlas', 'database', 'atlas'),
+        'user': get_var('atlas', 'user', 'atlas'),
+        'password': get_var('atlas', 'password', 'insecure')
+    },
+    'NAP': {
+        'host': get_var('nap', 'host', 'localhost'),
+        'port': get_var('nap', 'port', 5401),
+        'database': get_var('nap', 'database', 'nap'),
+        'user': get_var('nap', 'user', 'nap'),
+        'password': get_var('nap', 'password', 'insecure')
+    },
+    'MILIEU': {
+        'host': get_var('milieu', 'host', 'localhost'),
+        'port': get_var('milieu', 'port', 5402),
+        'database': get_var('milieu', 'database', 'milieuthemas'),
+        'user': get_var('milieu', 'user', 'milieuthemas'),
+        'password': get_var('milieu', 'password', 'insecure')
+    },
+    'TELLUS': {
+        'host': get_var('tellus', 'host', 'localhost'),
+        'port': get_var('tellus', 'port', 5409),
+        'database': get_var('tellus', 'database', 'tellus'),
+        'user': get_var('tellus', 'user', 'tellus'),
+        'password': get_var('tellus', 'password', 'insecure')
+    }
+} if in_docker() else {
+    'ATLAS': {
+        'host': get_var('atlas', 'host', 'atlas_db'),
+        'port': get_var('atlas', 'port', 5432),
+        'database': get_var('atlas', 'database', 'atlas'),
+        'user': get_var('atlas', 'user', 'atlas'),
+        'password': get_var('atlas', 'password', 'insecure')
+    },
+    'NAP': {
+        'host': get_var('nap', 'host', 'nap_db'),
+        'port': get_var('nap', 'port', 5432),
+        'database': get_var('nap', 'database', 'nap'),
+        'user': get_var('nap', 'user', 'nap'),
+        'password': get_var('nap', 'password', 'insecure')
+    },
+    'MILIEU': {
+        'host': get_var('milieu', 'host', 'milieu_db'),
+        'port': get_var('milieu', 'port', 5432),
+        'database': get_var('milieu', 'database', 'milieuthemas'),
+        'user': get_var('milieu', 'user', 'milieuthemas'),
+        'password': get_var('milieu', 'password', 'insecure')
+    },
+    'TELLUS': {
+        'host': get_var('tellus', 'host', 'tellus_db'),
+        'port': get_var('tellus', 'port', 5432),
+        'database': get_var('tellus', 'database', 'tellus'),
+        'user': get_var('tellus', 'user', 'tellus'),
+        'password': get_var('tellus', 'password', 'insecure')
+    }
+}
 
 
 DSN_ATLAS = 'postgresql://{}:{}@{}:{}/{}'.format(
-    os.getenv('ATLAS_DB_USER', 'atlas'),
-    os.getenv('ATLAS_DB_PASSWORD', 'insecure'),
-    os.getenv('ATLAS_DB_PORT_5432_TCP_ADDR', get_docker_host()),
-    os.getenv('ATLAS_DB_PORT_5432_TCP_PORT', 5405),
-    os.getenv('ATLAS_DB_NAME', 'atlas'),
+    DB_SETTINGS['ATLAS']['user'],
+    DB_SETTINGS['ATLAS']['password'],
+    DB_SETTINGS['ATLAS']['host'],
+    DB_SETTINGS['ATLAS']['port'],
+    DB_SETTINGS['ATLAS']['database']
 )
 
 DSN_NAP = 'postgresql://{}:{}@{}:{}/{}'.format(
-    os.getenv('NAP_DB_USER', 'nap'),
-    os.getenv('NAP_DB_PASSWORD', 'insecure'),
-    os.getenv('NAP_DB_PORT_5432_TCP_ADDR', get_docker_host()),
-    os.getenv('NAP_DB_PORT_5432_TCP_PORT', 5401),
-    os.getenv('NAP_DB_NAME', 'nap'),
+    DB_SETTINGS['NAP']['user'],
+    DB_SETTINGS['NAP']['password'],
+    DB_SETTINGS['NAP']['host'],
+    DB_SETTINGS['NAP']['port'],
+    DB_SETTINGS['NAP']['database']
 )
 
 DSN_MILIEU = 'postgresql://{}:{}@{}:{}/{}'.format(
-    os.getenv('MILIEU_DB_USER', 'milieuthemas'),
-    os.getenv('MILIEU_DB_PASSWORD', 'insecure'),
-    os.getenv('MILIEU_DB_PORT_5432_TCP_ADDR', get_docker_host()),
-    os.getenv('MILIEU_DB_PORT_5432_TCP_PORT', 5402),
-    os.getenv('MILIEU_DB_NAME', 'milieuthemas'),
+    DB_SETTINGS['MILIEU']['user'],
+    DB_SETTINGS['MILIEU']['password'],
+    DB_SETTINGS['MILIEU']['host'],
+    DB_SETTINGS['MILIEU']['port'],
+    DB_SETTINGS['MILIEU']['database']
 )
 
 DSN_TELLUS = 'postgresql://{}:{}@{}:{}/{}'.format(
-    os.getenv('TELLUS_DB_USER', 'tellus'),
-    os.getenv('TELLUS_DB_PASSWORD', 'insecure'),
-    os.getenv('TELLUS_DB_PORT_5432_TCP_ADDR', get_docker_host()),
-    os.getenv('TELLUS_DB_PORT_5432_TCP_PORT', 5409),
-    os.getenv('TELLUS_DB_NAME', 'tellus'),
+    DB_SETTINGS['TELLUS']['user'],
+    DB_SETTINGS['TELLUS']['password'],
+    DB_SETTINGS['TELLUS']['host'],
+    DB_SETTINGS['TELLUS']['port'],
+    DB_SETTINGS['TELLUS']['database']
 )
