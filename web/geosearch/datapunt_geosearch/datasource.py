0e4ae671a45c14c6988c69005b4313b8dab4609b
Bart Heupers <bart.heupers@gmail.com>
2018-03-08 14:23:41 +0100

    Fix crash that is probably caused by invalid data :
    Mar  8 13:38:55 dc01-acc geosearch[700]:   File "./datapunt_geosearch/datasource.py", line 207, in execute_polygon_query
    Mar  8 13:38:55 dc01-acc geosearch[700]:     cur.execute(sql, (self.y, self.x) * 3)
    Mar  8 13:38:55 dc01-acc geosearch[700]:   File "/usr/local/lib/python3.6/site-packages/psycopg2/extras.py", line 144, in execute
    Mar  8 13:38:55 dc01-acc geosearch[700]:     return super(DictCursor, self).execute(query, vars)
    Mar  8 13:38:55 dc01-acc geosearch[700]: psycopg2.InternalError: First argument geometry could not be converted to GEOS: IllegalArgumentException: Invalid number of points
     in LinearRing found 3 - must be 0 or >= 4

--- web/geosearch/datapunt_geosearch/datasource.py
+++ web/geosearch/datapunt_geosearch/datasource.py
@@ -200,9 +200,10 @@ FROM {}
 WHERE {} && ST_Transform(ST_GeomFromText('POINT(%s %s)', 4326), 28992)
 AND
 ST_Contains({}, ST_Transform(ST_GeomFromText('POINT(%s %s)', 4326), 28992)) {}
+AND ST_IsValid({})
 ORDER BY distance
             """.format(
-                self.fields, self.meta['geofield'], table, self.meta['geofield'], self.meta['geofield'], self.extra_where
+                self.fields, self.meta['geofield'], table, self.meta['geofield'], self.meta['geofield'], self.extra_where, self.meta['geofield']
             )
             cur.execute(sql, (self.y, self.x) * 3)
         else:
