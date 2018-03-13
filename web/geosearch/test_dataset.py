f01c893f881f3a253a7365c860fa2cc5ffd6a016
Bart Heupers <bart.heupers@gmail.com>
2018-03-13 16:31:43 +0100

    Fix TestNapDataset for new dataset

--- web/geosearch/test_dataset.py
+++ web/geosearch/test_dataset.py
@@ -33,8 +33,8 @@ class TestBAGDataset(unittest.TestCase):
 
 class TestNapDataset(unittest.TestCase):
     def test_query(self):
-        x = 120535.2
-        y = 486376.3
+        x = 120364.0
+        y = 488156.3
 
         ds = datasource.NapMeetboutenDataSource(dsn=config.DSN_NAP)
         results = ds.query(x, y)
@@ -43,22 +43,22 @@ class TestNapDataset(unittest.TestCase):
         self.assertIn('distance', results['features'][0]['properties'])
 
     def test_query_radius_and_limit(self):
-        x = 120535.2
-        y = 486376.3
-        radius = 130
+        x = 120364.0
+        y = 488156.3
+        radius = 70
         limit = 1
 
         ds = datasource.NapMeetboutenDataSource(dsn=config.DSN_NAP)
 
         results = ds.query(x, y, radius=radius)
-        self.assertEqual(len(results['features']), 4)
+        self.assertEqual(len(results['features']), 5)
 
         results = ds.query(x, y, radius=radius, limit=limit)
         self.assertEqual(len(results['features']), 2)
 
     def test_query_wgs84(self):
-        x = 52.3641918658574
-        y = 4.88121013879857
+        x = 52.38018
+        y = 4.87851
 
         ds = datasource.NapMeetboutenDataSource(dsn=config.DSN_NAP)
         results = ds.query(x, y, rd=False)
@@ -66,14 +66,14 @@ class TestNapDataset(unittest.TestCase):
         self.assertEqual(len(results['features']), 1)
 
     def test_query_wgs84_radius(self):
-        x = 52.3641918658574
-        y = 4.88121013879857
-        radius = 130
+        x = 52.38018
+        y = 4.87851
+        radius = 70
 
         ds = datasource.NapMeetboutenDataSource(dsn=config.DSN_NAP)
         results = ds.query(x, y, rd=False, radius=radius)
 
-        self.assertEqual(len(results['features']), 4)
+        self.assertEqual(len(results['features']), 5)
 
 
 class TestMunitieDataset(unittest.TestCase):
