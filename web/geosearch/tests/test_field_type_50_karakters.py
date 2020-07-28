145d340fab3276294765e9f7ceb0c8a1c0ff01bc
Chris van Riel <c.van.riel@amsterdam.nl>
2020-07-28 13:01:41 +0200

    adjusted test to other DSN

--- web/geosearch/tests/test_field_type_50_karakters.py
+++ web/geosearch/tests/test_field_type_50_karakters.py
@@ -13,26 +13,26 @@ class TestFieldType50Karakters(unittest.TestCase):
         # table_name could be anything, fietsplaatjes was choosen as mediator
         test_row = dict(
             schema="public",
-            table_name="fietspaaltjes_fietspaaltjes",
+            table_name="overlastgebieden",
             name="0123456789-0123456789-123",
-            name_field="id",
+            name_field="ogc_fid",
             geometry_type="POINT",
-            geometry_field="geometry",
-            id_field="id",
+            geometry_field="wkb_geometry",
+            id_field="ogc_fid",
             operator="contains",
             dataset_name="0123456789-0123456789-12",
         )          
 
         # setup context data
         registry = DatasetRegistry()
-        context_data = registry.init_dataset(row=test_row, class_name="TestDataset", dsn_name=config.DSN_DATASERVICES_DATASETS)
+        context_data = registry.init_dataset(row=test_row, class_name="TestDataset", dsn_name=config.DSN_VARIOUS_SMALL_DATASETS)
         
         # make connection and set vars
         dataset = DataSourceBase(connection=None, dsn=context_data.dsn_name)
         dataset.meta = context_data.metadata
         dataset.x = 123207
         dataset.y = 486624
-        dataset.radius = 50
+        dataset.radius = 500
         
         query_result = dataset.execute_queries()            
         
