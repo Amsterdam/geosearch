d2440154f725dc3aa8c08b8742d3c2aad40b4944
LB <78799392+lbam@users.noreply.github.com>
2022-09-06 16:01:57 +0200

    Fix download_dbs.sh
    Inserted the relevant parts of download-db.sh from the
    amsterdam/postgres Docker image.

--- download_dbs.sh
+++ download_dbs.sh
@@ -1,10 +1,13 @@
+#!/bin/sh
+
 # Download geosearch databases into /tmp/downloaded_dbs
+set -eu
+set -x
+
 rm -rf /tmp/downloaded_dbs
-rm /tmp/*_latest.gz
-for db in nap monumenten milieuthemas dataservices bag_v11 various_small_datasets;
-do
-  download-db.sh $db;
-done
 mkdir /tmp/downloaded_dbs
-mv /tmp/*_latest.gz /tmp/downloaded_dbs
-ls -lah /tmp/downloaded_dbs
+for db in nap monumenten milieuthemas dataservices bag_v11 various_small_datasets; do
+    url="https://admin.data.amsterdam.nl/postgres/${db}_latest.gz"
+    target="/tmp/downloaded_dbs/${db}_latest.gz"
+    wget -O "$target" -nc "$url"
+done
