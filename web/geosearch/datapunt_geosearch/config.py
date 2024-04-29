57ff3ed9d3fc245ccb96a776713502f55e7a55a2
jjmurre <jan.murre@gmail.com>
2024-04-29 15:33:29 +0200

    Remove unused code that uses removed legacy datasets.

--- web/geosearch/datapunt_geosearch/config.py
+++ web/geosearch/datapunt_geosearch/config.py
@@ -1,26 +1,10 @@
 import logging
 
+from datapunt_geosearch.base_config import DATAPUNT_API_URL  # noqa
 from datapunt_geosearch.base_config import (  # noqa: F401
-    CLOUD_ENV,
     DATABASE_SET_ROLE,
-    DATAPUNT_API_URL,
-    DEFAULT_SEARCH_DATASETS,
-    DSN_ADMIN_USER,
-    JW_KEYSET,
-    JWKS,
-    JWKS_SIGNING_ALGORITHMS,
-    JWKS_TEST_KEY,
-    JWKS_URL,
     db_connection_string,
     get_db_settings,
-)
-
-DSN_BAG = db_connection_string.format(**get_db_settings("bag_v11"))
-DSN_NAP = db_connection_string.format(**get_db_settings("nap"))
-DSN_MILIEU = db_connection_string.format(**get_db_settings("milieuthemas"))
-DSN_MONUMENTEN = db_connection_string.format(**get_db_settings("monumenten"))
-DSN_VARIOUS_SMALL_DATASETS = db_connection_string.format(
-    **get_db_settings("various_small_datasets")
 )
 
 dataservices_settings = get_db_settings("dataservices")
@@ -30,18 +14,8 @@ DATASERVICES_USER = dataservices_settings["username"]
 
 logging.debug(
     "\n\nApp Database config:\n\
-         Bag: %s\n\
-         Nap: %s\n\
-         Milieu: %s\n\
-         Monumenten: %s\n\
-        Various Small Datasets: %s\n\
         Dataservices: %s\n\
         Role switching %s\n\n",
-    DSN_BAG,
-    DSN_NAP,
-    DSN_MILIEU,
-    DSN_MONUMENTEN,
-    DSN_VARIOUS_SMALL_DATASETS,
     DSN_DATASERVICES_DATASETS,
     "active" if DATABASE_SET_ROLE else "inactive",
 )
--- web/geosearch/datapunt_geosearch/test_config.py
+++ web/geosearch/datapunt_geosearch/test_config.py
@@ -1,30 +1,7 @@
 import logging
 
-from datapunt_geosearch.base_config import (  # noqa: F401
-    CLOUD_ENV,
-    DATAPUNT_API_URL,
-    DEFAULT_SEARCH_DATASETS,
-    JW_KEYSET,
-    JWKS,
-    JWKS_SIGNING_ALGORITHMS,
-    JWKS_TEST_KEY,
-    JWKS_URL,
-    db_connection_string,
-    get_db_settings,
-)
+from datapunt_geosearch.base_config import db_connection_string, get_db_settings  # noqa: F401
 
-# db entries are overridden
-DSN_BAG = db_connection_string.format(**get_db_settings("bag_v11") | {"db": "test_bag_v11"})
-DSN_NAP = db_connection_string.format(**get_db_settings("nap") | {"db": "test_nap"})
-DSN_MILIEU = db_connection_string.format(
-    **get_db_settings("milieuthemas") | {"db": "test_milieuthemas"}
-)
-DSN_MONUMENTEN = db_connection_string.format(
-    **get_db_settings("monumenten") | {"db": "test_monumenten"}
-)
-DSN_VARIOUS_SMALL_DATASETS = db_connection_string.format(
-    **get_db_settings("various_small_datasets") | {"db": "test_various_small_datasets"}
-)
 DSN_DATASERVICES_DATASETS = db_connection_string.format(
     **get_db_settings("dataservices") | {"db": "test_dataservices"}
 )
@@ -33,17 +10,7 @@ ENV = "test"
 
 logging.debug(
     "\n\nApp test Database config:\n\
-         Bag: %s\n\
-         Nap: %s\n\
-         Milieu: %s\n\
-         Monumenten: %s\n\
-        Various Small Datasets: %s\n\
         Dataservices: %s\n\n",
-    DSN_BAG,
-    DSN_NAP,
-    DSN_MILIEU,
-    DSN_MONUMENTEN,
-    DSN_VARIOUS_SMALL_DATASETS,
     DSN_DATASERVICES_DATASETS,
 )
 
