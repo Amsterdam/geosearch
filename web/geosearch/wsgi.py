b5c611d74970240f76d2a865e547510fc1a582f8
jjmurre <jan.murre@gmail.com>
2024-04-29 14:06:01 +0200

    Add `--reload` flag to wsgi.py for easier local development.

--- web/geosearch/wsgi.py
+++ web/geosearch/wsgi.py
@@ -1,7 +1,13 @@
+import argparse
+
 from datapunt_geosearch import create_app
 
 app = create_app("datapunt_geosearch.config")
 
 
 if __name__ == "__main__":
-    app.run()
+
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument("--reload", dest="reload", action="store_true")
+    args = parser.parse_args()
+    app.run(debug=args.reload)
