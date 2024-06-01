2b812b7dc2449567ebef6966f80db47c95f393ce
Yashar Robert <yashar@xs4all.nl>
2024-06-02 01:58:13 +0200

    Remove old Jenkins scripts

--- db-scripts/download-dbs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Download geosearch databases into /tmp
-rm /tmp/*_latest.gz
-for db in nap monumenten milieuthemas dataservices bag_v11 various_small_datasets;
-do
-  download-db.sh $db;
-done
-ls -lah /tmp/*_latest.gz
