68c3e4e3b4cb4446ca2f37198c61dc384edc5031
Yashar Robert <yashar@xs4all.nl>
2024-06-02 01:54:21 +0200

    Update docker compose file
    * Remove references to old databases
    * Upgrade postgres container to v14
    * Add Dockerfile for development
    * Update Readme

--- db-scripts/init-dbs.sh
+++ db-scripts/init-dbs.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# Script for downloading, installing and configuring the roles for
-# databases used in development and tests of geosearch
-for db in nap monumenten milieuthemas dataservices bag_v11 various_small_datasets;
-do
-echo "Downloading $db"
-download-db.sh $db;
-echo "Restoring $db"
-update-db.sh $db;
-echo "Granting access on $db"
-psql -U postgres -d $db -c "GRANT ALL ON DATABASE $db TO $POSTGRES_USER;";
-psql -U postgres -d $db -c "CREATE DATABASE test_$db WITH TEMPLATE $db OWNER $POSTGRES_USER;";
-done
+# Add extra database for running tests
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE test_dataservices;
+    \c test_dataservices;
+    CREATE EXTENSION postgis;
+	GRANT ALL PRIVILEGES ON DATABASE test_dataservices TO postgres;
+EOSQL
