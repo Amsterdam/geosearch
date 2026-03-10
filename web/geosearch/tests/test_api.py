d05244b58905920ada8501d5ded53ea8dd8363a0
Jasper Jager <j.jager@amsterdam.nl>
2026-03-10 14:03:12 +0100

    Clear directory to start fresh

--- web/geosearch/tests/test_api.py
+++
@@ -1,5 +0,0 @@
-def test_cors_header(test_client):
-    with test_client() as client:
-        resp = client.get("/nap/?lat=52.7&lon=4.8")
-        assert "Access-Control-Allow-Origin" in resp.headers
-        assert resp.headers["Access-Control-Allow-Origin"] == "*"
